aef3bcc3f71e0d6bd7fb5d16efe070f46cb040f0
Binayak Rijal <emperorprotoss@gmail.com>
2026-02-25 10:39:28 +0545

    Update AboutScreen

--- app/src/main/java/com/example/travelsafe/view/AboutScreen.kt
+++ app/src/main/java/com/example/travelsafe/view/AboutScreen.kt
@@ -19,7 +19,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.travelsafe.R
-
+//DEbugged
 @Composable
 fun AboutScreen(onBackClick: () -> Unit = {}) {
     val scrollState = rememberScrollState()
