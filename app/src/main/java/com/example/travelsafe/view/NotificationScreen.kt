1a1bd4b8acda2ce0887eafd6d6de832d5e93d6a2
Binayak Rijal <emperorprotoss@gmail.com>
2026-02-25 10:39:37 +0545

    Update NotificationScreen

--- app/src/main/java/com/example/travelsafe/view/NotificationScreen.kt
+++ app/src/main/java/com/example/travelsafe/view/NotificationScreen.kt
@@ -19,7 +19,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.travelsafe.R
 import com.example.travelsafe.model.AppNotification
-
+//DEbugged
 @Composable
 fun NotificationScreen(onBackClick: () -> Unit = {}) {
     val notifications = remember {
